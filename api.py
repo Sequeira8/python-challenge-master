99e32ffd269741a537358c9b260bda733b40a3d0
Sequeira8 <carlos.m.r.sequeira@gmail.com>
2019-07-25 18:29:22 +0100

    Added ajax request to get the possible names from another app.route on flask where the input is read and we search for names in the Trie. Also the input of the list of names is given as an argument in the comand line and inserted in the main page. closes #3

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from wtforms import TextField, Form
 import autocomplete
+import csv
+import sys
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -15,12 +17,39 @@ class SearchForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def mainPage():
+
+    #List where the names in the input file will be stores
+    titles = []
+
+    #Open the input file and stores all the names in the titles list
+    with open(sys.argv[1], newline='\n') as file:
+        data = csv.reader(file)
+        for row in data:
+            titles.append(row[0])
+
+    #Insert Names in the Trie Data Structure
+    t.insertNames(titles)
+
     #Creates one form
     form = SearchForm(request.form)
 
     #Searches for appsearch.html in the templates folder and renders it
     #Also the form we created is sent
     return render_template("appsearch.html", title='App Search', form=form)
+
+
+@app.route('/_possibilities', methods=['POST'])
+def _possibilities():
+    #Receive input, searches for the possible names and returns a list of them
+
+    #Gets the User Input
+    input = request.form.get('input')
+
+    #List of the possible names
+    list = t.search(input)
+
+    #Transforms list to the desired format to present to the user
+    return jsonify({'data': render_template("namepossibilities.html", list=list)})
 
 
 #Error page in case the user tries to acess an undefined path
